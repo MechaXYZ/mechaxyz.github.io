5c47abc4e4253d26468f614fa16c2c4f15077b98
MechaXYZ <93439705+MechaXYZ@users.noreply.github.com>
2024-04-25 00:45:28 -0500

    add typewriter effect that took way too long #1

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-// Hello, world!
+var Typewriter = function(el, text, time, bold) {
+	this.target = text;
+	this.time = time
+	this.bold = bold;
+	this.txt = '';
+	this.el = el;
+	this.tick();
+};
+
+Typewriter.prototype.tick = function() {
+	this.txt = this.target.substring(0, this.txt.length + 1);
+
+	if (this.bold != "true") {
+		this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+	} else {
+		this.el.innerHTML = '<span class="wrap"><b>'+this.txt+'</b></span>';
+	}
+
+	var delta
+	var that = this;
+	
+	if (this.time) {
+		delta = (this.time / this.target.length) * 1000;
+	} else {
+		delta = 100;
+	}
+	
+	if (this.txt != this.target) {
+		setTimeout(function() {
+			that.tick();
+		}, delta);
+	}
+};
+
+window.onload = function() {
+	var elements = document.getElementsByClassName('typewrite');
+	for (var i=0; i<elements.length; i++) {
+		var text = elements[i].getAttribute('data-text');
+		var bold = elements[i].getAttribute('data-bold');
+		var time = parseFloat(elements[i].getAttribute('data-time'));
+
+		new Typewriter(elements[i], text, time, bold);
+	}
+};
